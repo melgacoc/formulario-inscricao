e4df01c74d4a279b5ade88738942528b7eefa898
Edvaldo Alves do Nascimento <edvaldodentt@gmail.com>
2022-06-09 14:49:58 -0300

    correções do Linter

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-function login (event) {
-    event.preventDefault();
-    const email = document.querySelector('#email');
-    const password = document.querySelector('#password');
-    if(email.value === 'tryber@teste.com' && password.value === '123456') {
-        alert('Olá, Tryber!');
-    }
-    else {
-        alert('Email ou senha inválidos.');
+function login(event) {
+  event.preventDefault();
+  const email = document.querySelector('#email');
+  const password = document.querySelector('#password');
+    if (email.value === 'tryber@teste.com' && password.value === '123456') {
+  alert('Olá, Tryber!');
+} else {
+  alert('Email ou senha inválidos.');
     }
 }
 const button = document.querySelector('#send-inf');
