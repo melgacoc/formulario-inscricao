8aebf2571e57533b96b074382504bea57cf1e279
Edvaldo Alves do Nascimento <edvaldodentt@gmail.com>
2022-06-10 06:00:21 -0300

    Inclusão de ação seletiva no botão

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ function login(event) {
 }
 const button = document.querySelector('#send-inf');
 button.addEventListener('click', login);
+
+document.getElementById("submit-btn").disabled = true;
+function disabled(event2) {
+  event2.preventDefault();
+  const button2 = document.getElementById("submit-btn");
+  const agreed = document.getElementById("agreement").value;
+  if (agreed !== null && agreed !== '') {
+    button2.disabled = false;
+  } else {
+    button2.disabled = true;
+  }
+}
+document.getElementById("agreement").addEventListener("input", disabled);
